c61cbf7830122a926c6cda34369dd53b51ca15d1
CO2NoTear <sqede@outlook.com>
2023-06-26 15:37:30 +0800

    log测试大体完成

--- tests/log_test.cc
+++ tests/log_test.cc
@@ -23,6 +23,13 @@ static std::string BigString(const std::string &partial_string, size_t n)
     return result;
 }
 
+static std::string NumberString(int x)
+{
+    char buf[32];
+    std::snprintf(buf, sizeof(buf), "%d.", x);
+    return std::string(buf);
+}
+
 TEST_CASE("writing")
 {
     SECTION("short string")
@@ -43,6 +50,76 @@ TEST_CASE("writing")
         std::string scrach;
         REQUIRE(reader_.ReadRecord(&record, &scrach));
         REQUIRE(record.ToString() == msg);
+    }
+    SECTION("long string")
+    {
+        // 观察是否有分块
+        PosixEnv *env_ = new PosixEnv;
+        WritableFile *dest;
+        SequentialFile *source;
+        std::string msg = BigString("hello ", 102400);
+        Status s = env_->NewWritableFile("testfile", &dest);
+        REQUIRE(s.ok());
+        Writer writer_(dest);
+        REQUIRE(writer_.AddRecord(Slice(msg)).ok());
+        s = env_->NewSequentialFile("testfile", &source);
+        REQUIRE(s.ok());
+        Reader::Reporter *repoter_;
+        Reader reader_(source, repoter_, true, 0);
+        Slice record;
+        std::string scrach;
+        REQUIRE(reader_.ReadRecord(&record, &scrach));
+        REQUIRE(record.ToString() == msg);
+    }
+    SECTION("many blocks")
+    {
+        // 生成1w个数字组成的字符串，全部写进log
+        PosixEnv *env_ = new PosixEnv;
+        WritableFile *dest;
+        SequentialFile *source;
+        Status s = env_->NewWritableFile("testfile", &dest);
+        REQUIRE(s.ok());
+        Writer writer_(dest);
+        for (int i = 1; i <= 100000; ++i) {
+            std::string msg = NumberString(i);
+            REQUIRE(writer_.AddRecord(Slice(msg)).ok());
+        }
+        s = env_->NewSequentialFile("testfile", &source);
+        REQUIRE(s.ok());
+        Reader::Reporter *repoter_;
+        Reader reader_(source, repoter_, true, 0);
+        Slice record;
+        std::string scrach;
+        for (int i = 1; i <= 100000; ++i) {
+            std::string msg = NumberString(i);
+            REQUIRE(reader_.ReadRecord(&record, &scrach));
+            REQUIRE(record.ToString() == msg);
+        }
+        // EOF
+        REQUIRE(!reader_.ReadRecord(&record, &scrach));
+    }
+    SECTION("aligned EOF")
+    {
+        // 填充整个block直到只剩下4字节，使得最后部分被00填充
+        const int n = kBlockSize - 2 * kHeaderSize + 3;
+        PosixEnv *env_ = new PosixEnv;
+        WritableFile *dest;
+        SequentialFile *source;
+        std::string msg = BigString("hello", n);
+        Status s = env_->NewWritableFile("testfile", &dest);
+        REQUIRE(s.ok());
+        Writer writer_(dest);
+        REQUIRE(writer_.AddRecord(Slice(msg)).ok());
+        s = env_->NewSequentialFile("testfile", &source);
+        REQUIRE(s.ok());
+        Reader::Reporter *repoter_;
+        Reader reader_(source, repoter_, true, 0);
+        Slice record;
+        std::string scrach;
+        REQUIRE(reader_.ReadRecord(&record, &scrach));
+        REQUIRE(record.ToString() == msg);
+        // EOF read
+        REQUIRE(!reader_.ReadRecord(&record, &scrach));
     }
 }
 
